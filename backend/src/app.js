82debbeb1783199380e60018dfa574933c742099
yash verma <63188505+yash3000@users.noreply.github.com>
2023-04-28 23:21:35 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,14 @@
 const express = require("express");
-// const compression = require("compression");
-// const cors = require("cors");
 const httpStatus = require("http-status");
 const routes = require("./routes");
-// const { errorHandler } = require("./middlewares/error");
 const ApiError = require("./utils/ApiError");
 const app = express();
-// const { jwtStrategy } = require("./config/passport");
-// const helmet = require("helmet");
-// const passport = require("passport");
-
-// set security HTTP headers - https://helmetjs.github.io/
-// app.use(helmet());
 
 // parse json request body
 app.use(express.json());
 
 // parse urlencoded request body
 // app.use(express.urlencoded({ extended: true }));
-
-// gzip compression
-// app.use(compression());
-
-// enable cors
-// app.use(cors());
-// app.options("*", cors());
-
-// Done: CRIO_TASK_MODULE_AUTH - Initialize passport and add "jwt" authentication strategy
-// app.use(passport.initialize());
-// passport.use('jwt',jwtStrategy);
 
 // Reroute all API request starting with "/v1" route
 app.use("/v1", routes);
@@ -37,8 +17,5 @@ app.use("/v1", routes);
 app.use((req, res, next) => {
     next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
 });
-
-// handle error
-// app.use(errorHandler);
 
 module.exports = app;
